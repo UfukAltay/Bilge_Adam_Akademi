fe28e3cfac5c4021f5f9f576d6c35c993d7994b5
Ufuk ALTAY <ufk.altay@gmail.com>
2022-04-13 17:44:13 +0300

    ilk alıştırma yapıldı

---
+++ Example1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // yorum satır.. derleyici yorum satırlarını dikkate almaz
+
+
+            //name özelliği kontrol için benzersiz bir özelliktir.. kontrolleri name özelliği sayedinde tanırız..name özelliğindeki değerler benzersiz olmalıdır...
+
+
+            // = atama operatörüdür... sağdaki değeri soldaki değere atar...
+            // atama operatörü katı bu operatördür...sağdaki değerle sodaki değer eşit olmak zorundadır...
+
+            //  bir kontrolün (buttonun) özelliklerine gidebilmek için . kullanırız (kontrol.ozellik gibi)
+
+
+            // c# büyük-küçük harf duyarlılığı olan bir prg. dilidir... yani nasıl tanımlandıysa öyle çağrılmak zorundadır...
+
+            // c#'da komut satırları ; ile sonlandırılmalıdır...
+
+            // c#'da açılan herşey kapatılmalıdır.. bu genel bir kuraldır....,
+            
+            button1.Text = "Tıklandı";
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            button1.Text = "Üzerime gelme";
+        }
+    }
+}
