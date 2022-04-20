d34d0623d6ef3e2bd5df0491260cd0af798ca3ef
Ufuk ALTAY <ufk.altay@gmail.com>
2022-04-20 13:57:14 +0300

    Example 2 completed

---
+++ c-Example2/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace c_Example2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //= sağdaki değeri sola atar
+
+            label1.Text = textBox1.Text;
+        }
+    }
+}
