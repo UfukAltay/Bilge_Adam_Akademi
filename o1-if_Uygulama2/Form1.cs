e18978d330c7d44db547b45ee79afb5b8d65a1cb
Ufuk ALTAY <ufk.altay@gmail.com>
2022-04-25 17:05:27 +0300

    if ile farklı bir uygulama daha yapıldı

---
+++ o1-if_Uygulama2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace o1_if_Uygulama2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "1")
+            {
+                MessageBox.Show("Pazartesi");
+            }
+            else if (textBox1.Text == "2")
+            {
+                MessageBox.Show("Salı");
+            }
+            else if (textBox1.Text == "3")
+            {
+                MessageBox.Show("Çarşamba");
+            }
+            else if (textBox1.Text == "4")
+            {
+                MessageBox.Show("Perşembe");
+            }
+            else if (textBox1.Text == "5")
+            {
+                MessageBox.Show("Cuma");
+            }
+            else if (textBox1.Text == "6")
+            {
+                MessageBox.Show("Cumartesi");
+            }
+            else if (textBox1.Text == "7")
+            {
+                MessageBox.Show("Pazar");
+            }
+            else
+            {
+                MessageBox.Show("Geçerli bir tuşa basınız...");
+            }
+        }
+    }
+}
