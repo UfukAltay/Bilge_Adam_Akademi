bdbdddd73f41826e6061fdade4c9374d057ecc73
Ufuk ALTAY <ufk.altay@gmail.com>
2022-04-26 19:37:25 +0300

    Listbox ve Combobox konuları işlendi

---
+++ q-Listbox-ComboboxKontrolleri/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace q_Listbox_ComboboxKontrolleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(62, 58);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(245, 23);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(62, 122);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(245, 274);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ q-Listbox-ComboboxKontrolleri/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace q_Listbox_ComboboxKontrolleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // object c#'da herşeyin(bütün tiplerin) atasıdır...yani c#'da herşey birer object'dir...farklı tiplerdeki değerler object'de saklanabilir...
+
+            // boxing => kutulama
+            object o = "C# dersleri";
+            int sayi = 4;
+            // boxing => kutulama
+            object obj = sayi;
+
+            listBox1.Items.Add("Ekrem");
+            listBox1.Items.Add('A');
+            listBox1.Items.Add(44);
+            listBox1.Items.Add(false);
+            listBox1.Items.Add(4.1f);
+
+            // combobox'a eleman ekleme
+            comboBox1.Items.Add("Ekrem Yıldırım");
+            comboBox1.Items.Add("Samet");
+            comboBox1.Items.Add("Tayfun");
+            comboBox1.Items.Add("Burak");
+            comboBox1.Items.Add("Davut");
+            comboBox1.Items.Add("Celal");
+            comboBox1.Items.Add("Ufuk");
+            comboBox1.Items.Add("Baturay");
+            comboBox1.Items.Add("Yiğit");
+            comboBox1.Items.Add("Gürkan");
+            comboBox1.Items.Add("Tunay");
+            comboBox1.Items.Add("Barış");
+            comboBox1.Items.Add("Simge");
+        }
+    }
+}
