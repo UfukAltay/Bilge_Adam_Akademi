b4ab4a681bca7a754f796e5cbe3487d2e7dd3839
Ufuk ALTAY <ufk.altay@gmail.com>
2022-05-05 21:14:44 +0300

    Array sınıfı methodlarıyla ilgili başka bir örnek daha yapıldı.

---
+++ 1-Temel_Programlama/t5-ArraySinifi_Methodlari3/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace t5_ArraySinifi_Methodlari3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // sizin yapacağınız girilen değer 0-20 arasında olacak...
+            // bu işlemleri yapıp arama yaparken indexof methoduu ile yapacaksınız...
+            listBox1.Items.Clear();
+            int[] dizi = new int[10];
+            Random rnd = new Random();
+            // dizinin elemanlarını ekleme
+            for (int i = 0; i < 10; i++)
+            {
+                dizi[i] = rnd.Next(0, 20);
+            }
+
+            foreach (int s in dizi)
+            {
+                listBox1.Items.Add(s);
+            }
+
+            int sayi = Convert.ToInt32(textBox1.Text);
+            // dizi içerisind girilen sayıyı arama
+            int adet = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (dizi[i] == sayi)
+                {
+                    adet++;
+                }
+
+            }
+
+            label1.Text = adet + " sayı tahmin ettiniz";
+        }
+    }
+}
