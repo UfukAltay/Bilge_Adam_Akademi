838455b8cdfbeb771555820b1902eb3682033db1
Ufuk ALTAY <ufk.altay@gmail.com>
2022-05-08 13:31:12 +0300

    Arrays Telefon Defteri uygulaması yapıldı

---
+++ 1-Temel_Programlama/t7-Arrays-TelefonDefteriApp/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace t7_Arrays_TelefonDefteriApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtTelNo = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lstRehber = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad Soyad :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Telefon No  :";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(125, 39);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(214, 23);
+            this.txtAdSoyad.TabIndex = 2;
+            // 
+            // txtTelNo
+            // 
+            this.txtTelNo.Location = new System.Drawing.Point(125, 78);
+            this.txtTelNo.Name = "txtTelNo";
+            this.txtTelNo.Size = new System.Drawing.Size(214, 23);
+            this.txtTelNo.TabIndex = 3;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnEkle.Location = new System.Drawing.Point(248, 124);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(91, 32);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // lstRehber
+            // 
+            this.lstRehber.FormattingEnabled = true;
+            this.lstRehber.ItemHeight = 15;
+            this.lstRehber.Location = new System.Drawing.Point(43, 181);
+            this.lstRehber.Name = "lstRehber";
+            this.lstRehber.Size = new System.Drawing.Size(296, 304);
+            this.lstRehber.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(240, 491);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 49);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Göster";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(43, 534);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 51);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Göster";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 597);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstRehber);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtTelNo);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.TextBox txtTelNo;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox lstRehber;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ 1-Temel_Programlama/t7-Arrays-TelefonDefteriApp/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace t7_Arrays_TelefonDefteriApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] Adsoyad = new string[0];
+
+        string[] Telefon = new string[0];
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            Array.Resize(ref Adsoyad, Adsoyad.Length + 1);
+            Adsoyad[Adsoyad.Length - 1] = txtAdSoyad.Text;
+            Array.Resize(ref Telefon, Telefon.Length + 1);
+            Telefon[Telefon.Length - 1] = txtTelNo.Text;
+
+            //2.boyutlu diziye eleman tanımlama
+            TelefonDefteri[0, 0] = txtAdSoyad.Text;
+            TelefonDefteri[0, 1] = txtTelNo.Text;
+            //int sayi=  TelefonDefteri.Length;
+
+            txtAdSoyad.Clear();
+            txtTelNo.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < Telefon.Length; i++)
+            {
+                lstRehber.Items.Add(Adsoyad[i] + " " + Telefon[i]);
+            }
+
+
+            //lstRehber.Items.AddRange(Adsoyad);rakamsal dizilerde hata verir.
+
+            //foreach (string item in Adsoyad)
+            //{
+            //    lstRehber.Items.Add(item);
+            //}
+
+
+            /* 2 Boyutlu dizilerde ekleme işlemi*/
+
+            //for (int i = 0; i < TelefonDefteri.Length; i++)
+            //{
+            //    for (int j = 0; j < 2; j++)
+            //    {
+            //        lstRehber.Items.Add(TelefonDefteri[i, j]);
+            //    }
+            //}
+
+        }
+
+        //2 boyutlu dizi
+        string[,] TelefonDefteri = new string[50, 2];
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Düzensiz dizi
+            string[][,] aa = new string[6][,]{
+
+            new string[,] {{"","",""}},
+
+            new string[,] {{"","","",""} },
+
+            new string[,] {{"","",""}},
+
+            new string[,] {{"","",""}},
+
+            new string[,] {{"","",""}},
+
+             new string[,] {{"","",""}},
+           };
+
+            int a = aa[1].Length;
+        }
+    }
+}
