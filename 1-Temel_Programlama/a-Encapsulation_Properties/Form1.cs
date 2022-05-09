41ca83bb798cfab2445d6f74c1db5a1e4ac6400c
Ufuk ALTAY <ufk.altay@gmail.com>
2022-05-09 22:51:53 +0300

    OOP Giriş yapıldı, Encapsullation (Kapsülleme) konusu işlendi

---
+++ 1-Temel_Programlama/a-Encapsulation_Properties/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace a_Encapsulation_Properties
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ogrenci ogr = new Ogrenci();
+            ogr.Adi = "Yiğit";
+            ogr.SoyAdi = "Yeprem";
+            ogr.VizeNotu = 40;
+            ogr.FinalNotu = 50;
+
+
+            Ogrenci ogr1 = new Ogrenci();
+            ogr1.Adi = "DaVuT";
+            ogr1.SoyAdi = "Avcı";
+            ogr1.VizeNotu = 60;
+            ogr1.FinalNotu = 50;
+
+            //ogr1.Ortalama = 90; bu atama işlemi hatalı olacaktır... çünkü Ortalama propertysinin set bloğu kaldırıldığı için sadece okunabilir bir özelliktir..
+
+            MessageBox.Show(ogr.Ortalama.ToString());
+            MessageBox.Show(ogr1.Ortalama.ToString());
+        }
+    }
+}
---
+++ 1-Temel_Programlama/a-Encapsulation_Properties/Ogrenci.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a_Encapsulation_Properties
+{
+    public class Ogrenci
+    {
+        private string adi;
+        public string Adi
+        {
+            // değer okumak için kullanılır 
+
+            get
+            {
+                return adi.ToUpper();
+            }
+            set
+            {
+                #region Okuyun
+                // değer atamak için kullanılır..
+                // value parametredir.. property'e atanan değerdir.. atanan değeri adi fieldında saklıyoruz...properynin tipi neyse value tipide odur...     
+                #endregion
+                adi = value;
+            }
+        }
+
+        // propertyler fieldlara dışarıdan erişmek için kullanılırlar..propertyleri SoyAdi propertysi kullanırsak kendisi bir gizli field tanımlar
+        public string SoyAdi { get; set; }
+
+        public float VizeNotu { get; set; }
+
+        public float FinalNotu { get; set; }
+
+        public float Ortalama
+        {
+            get
+            {
+                return VizeNotu * 0.4f + FinalNotu * 0.6f;
+            }
+        }
+
+        #region Okuyun
+
+
+        // properties
+
+        // sınıf üyesidir... sınıfın özellikleridir...fieldlara dışarıdan kontrollü erişim için vardır... c#'da 3.0 dan önce javadaki gibi bu methodlarla yapılırdı.. 3.0 'dan sonra ise propertylerle yapılır...propertyler fieldların daha gelişmiş halidir... 
+
+
+        // Sınıf üyelerine sınıf seviyesinde erişilemez..Sınıf üyelerine sadece sınıfın alt scoparında erişebiliriz...bu alt scopelar method veya properties'lerdir
+
+        //public float Ortalama = VizeNotu
+        #endregion
+    }
+}
