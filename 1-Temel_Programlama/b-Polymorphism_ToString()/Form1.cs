4f32a47ea0ab2f134c13caabd6eeaf0389060bd0
Ufuk ALTAY <ufk.altay@gmail.com>
2022-05-11 22:26:46 +0300

    Polymorphism konusu işlendi

---
+++ 1-Temel_Programlama/b-Polymorphism_ToString()/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace b_Polymorphism_ToString__
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ogrenci ogr = new Ogrenci();
+            ogr.Adi = "Yiğit";
+            ogr.SoyAdi = "Yeprem";
+
+            Ogrenci ogr1 = new Ogrenci();
+            ogr1.Adi = "Samet";
+            ogr1.SoyAdi = "Uzun";
+
+            Ogrenci ogr2 = new Ogrenci();
+            ogr2.Adi = "Celal";
+            ogr2.SoyAdi = "Helvacı";
+
+            listBox1.Items.Clear();
+
+
+            // nesnenin objecte dönüşümüne boxing denir
+            listBox1.Items.Add(ogr);
+            listBox1.Items.Add(ogr1);
+            listBox1.Items.Add(ogr2);
+
+            listBox1.Items.Add(button1);
+            listBox1.Items.Add("Ekrem");
+            // nesneler çalışma zamanında veya derlenme zamanında stringe çevrilirken Projeismi.classIsmi ile çevrilir
+
+            // derlenme zamanında listbox'a eleman eklerken object olarak eklenir...çalışma zamanında o elemanlar object olarak saklanır..son kullanıcıya gösterirkende string olarak gösterir...
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // objectin nesneye dönüşümüne unboxing denir
+            object seciliOgrenci = listBox1.SelectedItem;
+
+            // bu işlemi yaparken objecdeki tipin Ogrenci olduğuna emin olmalıyız...
+            // NOt: Object kullanımında tip güvenliği yoktur... hatalar runtimeda oluşurlar...
+            if (listBox1.SelectedItem is Ogrenci)
+            {
+                Ogrenci seciliOgr = (Ogrenci)listBox1.SelectedItem;
+                MessageBox.Show(seciliOgr.Adi);
+            }
+            else
+            {
+                MessageBox.Show(seciliOgrenci.ToString());
+            }
+        }
+    }
+}
---
+++ 1-Temel_Programlama/b-Polymorphism_ToString()/Ogrenci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace b_Polymorphism_ToString__
+{
+    public class Ogrenci
+    {
+        public string Adi { get; set; }
+        public string SoyAdi { get; set; }
+
+
+        // c#'da hazır bütün tipler ve bizim kullandığımız tipler birer objedir..
+
+        // obje bir sınıftır..bütün sınıflara gizli miras verir
+
+        public override string ToString()
+        {
+            //return "Ekrem YILDIRIM";
+            return Adi + " " + SoyAdi;
+        }
+    }
+}
