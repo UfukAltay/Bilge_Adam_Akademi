9158dc21b85e530bffeb050c368f6cd678d295fc
Ufuk ALTAY <ufk.altay@gmail.com>
2022-05-12 22:31:10 +0300

    Polymorphism Uygulama yapıldı

---
+++ 1-Temel_Programlama/b1-PolymorphisimUygulama/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace b1_PolymorphisimUygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Kedi k = new Kedi();
+
+            MessageBox.Show(k.CikardigiSes());
+
+            Kus ks = new Kus();
+
+            MessageBox.Show(ks.CikardigiSes());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HayvanlarAlemi[] alemci = new HayvanlarAlemi[2];
+
+            Kus ks = new Kus();
+            Kedi k = new Kedi();
+
+            //HayvanlarAlemi av = k;
+            alemci[0] = ks;
+            alemci[1] = k;
+            foreach (HayvanlarAlemi item in alemci)
+            {
+                MessageBox.Show(item.CikardigiSes());
+            }
+        }
+    }
+}
---
+++ 1-Temel_Programlama/b1-PolymorphisimUygulama/HayvanlarAlemi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace b1_PolymorphisimUygulama
+{
+    // Insan base class düşünce methodu var
+    // Personel,Patron,Hoca,Ogrenci sınıfları Insan'dan miras alıyor.. insan sınıfının düşünce methoduda miras alan sınıflar tarafındn gövdesi farklı şekilde yazılmalı.
+    public abstract class HayvanlarAlemi
+    {
+        public abstract string CikardigiSes();
+    }
+    public class Kedi : HayvanlarAlemi
+    {
+        public override string CikardigiSes()
+        {
+            return "miyav miyav";
+        }
+    }
+    public class Kus : HayvanlarAlemi
+    {
+        public override string CikardigiSes()
+        {
+            return "cik cik";
+        }
+    }
+}
